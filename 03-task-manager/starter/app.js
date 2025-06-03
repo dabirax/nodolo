8e01d974df282ae4642fc29f0b54b7b9514141bb
dabirax <oliverpraise1@gmail.com>
2025-06-03 09:00:20 +0100

    validation & database connection

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,12 +1,35 @@
+const connectDB = require("./db/connect");
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+require('dotenv').config()
 
-app.get('/hello', (req,res)=>{
-    res.send('Task Manager App')
-})
+// middleware
 
+app.use(express.json());
+
+//routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+//app.get('/api/v1/tasks') - get all the tasks
+// app.post('/api/v1/tasks') - create a new task
+// app.get('/api/v1/tasks/:id') - get single task
+// app.patch('/api/v1/tasks/:id') - update task
+// app.delete('/api/v1/tasks/:id') - delete task
 
 const port = 5175;
 
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.listen(port, console.log(`server is listening on ${port}`));
+start();
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,34 @@
+const Task = require("../models/Task");
+
+const getAllTasks = (req, res) => {
+  res.send("all items from the file");
+};
+
+const createTasks = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+
+const deleteTasks = (req, res) => {
+  res.send("delete task");
+};
+
+module.exports = {
+  getAllTasks,
+  createTasks,
+  getTask,
+  updateTask,
+  deleteTasks,
+};
