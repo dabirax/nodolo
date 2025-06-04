4018af28ad2d428d11b32f75108e3a4e31879bb2
dabirax <oliverpraise1@gmail.com>
2025-06-04 18:01:13 +0100

    seperating the authentication middleware

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -25,28 +25,15 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
-  // console.log(authHeader)
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
-    throw new CustomAPIError("No token provided", 401);
-  }
-
-  const token = authHeader.split(" ")[1];
-  // console.log(token);
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    // console.log(decoded);
-
+  console.log(req.user)
   const luckyNumber = Math.floor(Math.random() * 100);
+  
+          res.status(200).json({
+            msg: `Hello, ${req.user.username}`,
+            secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+          });
 
-  res.status(200).json({
-    msg: `Hello, ${decoded.username}`,
-    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-  });
-} catch (error) {
-    throw new CustomAPIError("Not authorised access to this route", 401);
-  }
+  
 }
 
 module.exports = { login, dashboard };
---
+++ 05-JWT-Basics/starter/middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const CustomAPIError = require("../errors/custom-error");
+
+const authenticationMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  // console.log(authHeader)
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+  // console.log(token);
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // console.log(decoded);
+
+    const { id, username } = decoded;
+    req.user = { id, username };
+    next();
+  } catch (error) {
+    throw new CustomAPIError("Not authorised access to this route", 401);
+  }
+
+  //   console.log(req.headers.authorization);
+};
+
+module.exports = authenticationMiddleware;
