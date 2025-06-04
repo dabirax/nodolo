653b9dde958e3ba5ba57fc3e7957423bcf1421a4
dabirax <oliverpraise1@gmail.com>
2025-06-04 21:08:09 +0100

    commit

---
+++ 06-jobs-api/starter/controllers/auth.js
@@ -0,0 +1,11 @@
+const User = require("../models/User");
+
+const register = async (req, res) => {
+  res.json(req.body);
+};
+
+const login = async (req, res) => {
+  res.send("login user");
+};
+
+module.exports = { register, login };
