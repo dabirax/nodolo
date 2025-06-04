7f0e120000448aa7596b561144e0c0b836871af1
dabirax <oliverpraise1@gmail.com>
2025-06-04 12:17:10 +0100

    queries and search params

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,9 +1,34 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "products testing route" });
+  products = await Product.find({}).sort("-name price");
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "product route" });
+  const { featured, company, name, sort } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+    // options: 'i' is for case insensitive
+  }
+  console.log(queryObject);
+
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    // products = products.sort();
+    console.log(sort)
+  }
+  const products = await result;
+  //   products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
