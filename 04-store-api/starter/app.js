2f7f38be2da5a6cef5ad499890159d7af4354125
dabirax <oliverpraise1@gmail.com>
2025-06-04 08:09:48 +0100

    waking up to push

---
+++ 04-store-api/starter/app.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+// async errors
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+const productsRouter = require('./routes/products')
+
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
+
+//middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send(
+    '<h1> Store API </h1> <a href ="/api/v1/products"> products route</a>'
+  );
+});
+
+app.use("/api/v1/products", productsRouter);
+
+// product route
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //connect DB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
