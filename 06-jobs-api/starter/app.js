a656345c2122d78c528b255ad8314bdc90d42358
dabirax <oliverpraise1@gmail.com>
2025-06-05 23:26:37 +0100

    Diving into security

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 require("express-async-errors");
+
+//extra security packageds
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
 // connect DB
 const connectDB = require("./db/connect");
-
 const authenticateUser = require("./middleware/authentication");
 
 //routers
@@ -17,6 +23,16 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, // limit each IP to 100 request
+  })
+);
 // extra packages
 
 // routes
