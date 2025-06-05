282caf3bea4fde7f18d507cb832d3cc9fe06578b
dabirax <oliverpraise1@gmail.com>
2025-06-05 02:29:30 +0100

    delete Job

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -46,6 +46,16 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send("delete job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findByIdAndDelete({ _id: jobId, createdBy: userId });
+
+  if (!job) {
+    throw new NotFoundError(`There is no job with Id: ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).send("Deletion successful");
 };
 module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
