15c2e78b815c33bd059be1fa73691cc2f66d8f4a
dabirax <oliverpraise1@gmail.com>
2025-06-05 21:16:33 +0100

    Validation Errors

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -24,7 +24,7 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    throw new BadRequestError("Please prvide email and password");
+    throw new BadRequestError("Please provide email and password");
   }
   const user = await User.findOne({ email });
   if (!user) {
